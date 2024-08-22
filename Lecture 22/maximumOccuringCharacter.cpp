0500c5f0af4d9d63940137c843a312cf2d19884c
Han <variety1174@gmail.com>
2024-08-22 23:22:20 +0530

    maximumOccCharacters

---
+++ Lecture 22/maximumOccuringCharacter.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char getMaxOccCharacter(string s){
+    int arr[26] = {0};
+
+    for(int i = 0;i<s.length();i++){
+        char ch = s[i];
+        //lowercase
+        int number = 0;
+        if(ch>='a'&&ch<='z'){
+            number = ch - 'a';
+        }
+        else{
+            number = ch - 'A';
+        }
+        arr[number]++;
+    }
+    int maxi = -1,ans = 0;
+    for(int i = 0;i<26;i++){
+        if(maxi<arr[i]){
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+    char finalAns = 'a'+ans;
+    return finalAns;
+}
+
+int main(){
+    string s;
+    cin>>s;
+    cout<<getMaxOccCharacter(s)<<endl;
+}
