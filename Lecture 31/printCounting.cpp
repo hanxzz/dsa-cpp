4b03e70b42838ce88c9877e840afd3fcf8eddf7f
Han <variety1174@gmail.com>
2024-09-16 00:56:22 +0530

    test

---
+++ Lecture 31/printCounting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int printCounting(int n){
+    //base case
+    if(n==0){
+        return 0;
+    }
+    cout<<n;
+    printCounting(n-1);
+    return n;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    printCounting(5);
+}
